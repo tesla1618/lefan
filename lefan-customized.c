ee870ac2d9e82335a395e9a41a118a9580bf752f
Tesla <67636538+tesla1618@users.noreply.github.com>
2021-11-30 13:21:19 +0600

    Update lefan-customized.c

--- lefan-customized.c
+++ lefan-customized.c
@@ -33,7 +33,7 @@ reg() {
     scanf("%c", &close);
     if (close == '\n') {
 		system("clear");
-     	in();
+     	wlc();
     }
 
 }
@@ -425,6 +425,73 @@ dis_2()
 }
 
 
+
+ntgrate () {
+    int a=0,b=0,c=0,d,e,mp,n,var,terms,i,j;
+    char chars, sign1, sign2,sign,sign3,sign4,sign5,sign6,sign7,sign8,sign9,sign10,sign11;
+    char powr;
+    printf("\n\033[35m\t\e[1mArea binded by limit under a curve\033[0m\e[0m\n");
+    printf("\n");
+    printf("\tEnter the power (n) of x (x^n): ");
+    scanf("%d",&mp);
+    terms = mp+1;
+    int ara[terms],cont;
+    printf("\tEnter %d Constant value with x (if no, enter 1): ");
+    scanf("%d",&cont);
+
+
+
+    printf("\n\t--------------------------\n");
+            printf("\tThe Equation is y = %dx^%d\n", cont,mp);
+            printf("\t--------------------------\n");
+            printf("\tINTEGRATION of Y:\n");
+            printf("\t--------------------------\n");
+            printf("\tint(y) dx = (%d/%d)*x^%d\n", cont,mp+1,mp+1);
+            printf("\t--------------------------\n");
+
+            /*
+            int ul,ll;
+            printf("Enter Upper Limit and Lower Limit: ");
+            scanf("%d %d",&ul,&ll);
+            double area=0,div,vx=0,vu=0,vl=0;
+            div = cont/mp+1;
+            for (int i = 1; i <= ul; i++) {
+                vu = vu*vu;
+            }
+            for (int i = 1; i <= ll; i++) {
+                vl = vl*vl;
+            }
+            vx = powr(vu,mp+1)
+            vx = vu-vl;
+            area = div*vx;
+            printf("Area binded by (%d , %d) is %.3lf\n",ul,ll,area);
+
+            */
+
+            printf("\nPress 1 to Calculate another, 2 to go back to options page and 0 to log out/exit\n");
+    int choose;
+    scanf("%d",&choose);
+
+    if (choose == 1) {
+        system("clear");
+        ntgrate();
+    }
+    if (choose == 2) {
+        system("clear");
+        wlc();
+    }
+    if (choose == 0) {
+        system("clear");
+        main();
+    }
+
+
+
+}
+
+
+
+
 diff() {
 
     int a=0,b=0,c=0,d,e,mp,n,var,terms,i,j;
@@ -659,12 +726,7 @@ angle()
 
 }
 
-ntgrate () {
-    int a=0,b=0,c=0,d,e,mp,n,var,terms,i,j;
 
-
-
-}
 
 
 isect() {
@@ -942,8 +1004,118 @@ invelyz () {
 
     // it took a good amount of calculation
 
+    printf("\nPress 1 to Calculate another, 2 to go back to options page and 0 to log out\n");
+int choose;
+    scanf("%d",&choose);
+
+    if (choose == 1) {
+            system("clear");
+        invelyz();
+    }
+    if (choose == 2) {
+        system("clear");
+        wlc();
+    }
+    if (choose == 0) {
+        system("clear");
+        main();
+    }
+
 
 }
+
+
+
+pdig() {
+
+    double price,oprice,pdiff;
+    int a,b,c,d,e,f,g,h,i,j,k,l;
+
+    printf("\n\tWelcome to Profit Diagram");
+    printf("\n\tIf you are running a shop and you have some good amount of\n\tcompetitors around you, calculate the\n\tprobability if you can attract more customers\n\tby your servuce!\n");
+
+    printf("\n\tEnter the price of your most iconic product: ");
+    scanf("%lf", &price);
+    printf("\n\n\tEnter 1 if yes/true and 0 if no/false:");
+    printf("\n\tIs there any other shop selling this product? ");
+    scanf("%d",&a);
+    if (a >0 ){
+    printf("\n\tWhats the price of the product they are selling for? ");
+    scanf("%lf",&oprice);
+    printf("\n\tDo they provide any warranty/after sales service? ");
+    scanf("%d",&b);
+    printf("\n\tDo you provide any warranty/after sales service? ");
+    scanf("%d",&c);
+    printf("\n\tAre they providing any offers with the product? ");
+    scanf("%d",&d);
+    printf("\n\tAre you providing any offers with the product? ");
+    scanf("%d",&e);
+    printf("\n\tWhich shop is more organized and\n\tconsumer friendly? ");
+    printf("Type 1 if yours, 0 if theirs\n");
+    scanf("%d",&f);
+    pdiff = oprice - price;
+
+    int fb,fc,fd,fe,ff,pl;
+    fb = b*30;
+    fc = c*30;
+    fd = d*40;
+    fe = e*40;
+    if (f == 1) {
+        ff = f*20;
+    }
+    if (f == 0) {
+        ff = 0;
+    }
+    if (pdiff > 0) {
+        pl = 10;
+    }
+    if (pdiff < 0 ) {
+        pl = -8;
+    }
+    if (pdiff == 0){
+        if (c+e+f > b+d+1) {
+            pl = 5;
+        }
+        if (c+e+f < b+d+1) {
+            pl = 0;
+        }
+    }
+
+    int sum = 0;
+    sum = fc + fe + ff+pl;
+    printf("\tYou will Attract %d percent of customers!\n",sum);
+    }
+    else {
+        printf("\n\tYou are the only player in the field, Play as you like!!\n");
+    }
+
+
+    printf("\nPress 1 to Calculate another, 2 to go back to options page and 0 to log out\n");
+int choose;
+    scanf("%d",&choose);
+
+    if (choose == 1) {
+            system("clear");
+        pdig();
+    }
+    if (choose == 2) {
+        system("clear");
+        wlc();
+    }
+    if (choose == 0) {
+        system("clear");
+        main();
+    }
+
+
+
+    printf("\n");
+
+}
+
+
+
+
 landing() {
     int ch;
     printf("\n\n\t\t\033[36mWelcome to Analysis of Functions and Linear Equations\033[0m\n\n");
@@ -955,7 +1127,7 @@ landing() {
 	printf("\t\t");scanf("%d", &ch);
 	if ( ch == 1) {
             system("clear");
-            wlc();
+            wlcanon();
             }
 	if (ch == 2) {
             system("clear");
@@ -970,8 +1142,8 @@ landing() {
 wlc() {
 
     char sentence[1000];
-    //FILE *history;
-    //history = fopen("calculation_history_universal.txt", "w");
+    FILE *history;
+    history = fopen("calculation_history_universal.txt", "w");
 	int opr;
 	printf("\n\t----------------\n\t      Menu\n\t----------------\n\n");
 	printf("\033[35m\t\e[1mCoordinate Geometry\033[0m\e[0m\n");
@@ -998,7 +1170,7 @@ wlc() {
     while ( opr > 15) {
 	printf("\t---------------------\n\tChoose Operation: "); scanf("%d",&opr);
 	printf("\r---------------------\n");
-	if (opr >= 15) {
+	if (opr > 15) {
 		printf("\n\n\tINVALID OPERATION!!!\n\n");
 		printf("\t---------------------\n\n\tChoose Operation: "); scanf("%d",&opr);
 	printf("\t---------------------\n");
@@ -1009,14 +1181,247 @@ wlc() {
 	}
 
 		if (opr == 1) {
-            //fputs(history, "User opened Distance between two points\n");
-            //fclose(history);
+            fputs(history, "User opened Distance between two points\n");
+            fclose(history);
 			system("clear");
 			dis_1();
 		}
 		if (opr == 2) {
-            //fputs(history, "User opened Cartesian to Polar Conversion\n");
-            //fclose(history);
+            fputs(history, "User opened Cartesian to Polar Conversion\n");
+            fclose(history);
+			system("clear");
+			pol();
+		}
+		if (opr == 3) {
+            fputs(history, "User opened Quadrant of a point\n");
+            fclose(history);
+			system("clear");
+			qua();
+		}
+		if (opr == 4) {
+		    fputs(history, "User opened Mirror of a point\n");
+            fclose(history);
+			system("clear");
+			mircor();
+		}
+		if (opr == 5) {
+		    fputs(history, "User opened Distance of a point from Origin\n");
+            fclose(history);
+			system("clear");
+			dis_2();
+		}
+		if (opr == 6) {
+		    fputs(history, "User opened Angle made by points\n");
+            fclose(history);
+			system("clear");
+			angle();
+		}
+		if (opr == 7) {
+		    fputs(history, "User opened Straight line equation generator\n");
+            fclose(history);
+			system("clear");
+			eqgen();
+		}
+		if (opr == 8) {
+		    fputs(history, "User opened Distance between two parallel straight lines\n");
+            fclose(history);
+			system("clear");
+			dis_st();
+		}
+		if (opr == 9) {
+		    fputs(history, "User opened Slope of a st line\n");
+            fclose(history);
+			system("clear");
+			slopeq();
+		}
+		if (opr == 10) {
+		    fputs(history, "User opened intersect points of st lines\n");
+            fclose(history);
+			system("clear");
+			isect();
+		}
+		if (opr == 11) {
+		    fputs(history, "User opened Maxima/Minima of a function\n");
+            fclose(history);
+			system("clear");
+			maxima();
+		}
+		if (opr == 12) {
+		    fputs(history, "User opened Profit diagram\n");
+            fclose(history);
+			system("clear");
+			diff();
+		}
+		if (opr == 13) {
+		    fputs(history, "User opened Integration of a Fucntion\n");
+            fclose(history);
+			system("clear");
+			ntgrate();
+		}
+		if (opr == 14) {
+		    fputs(history, "User opened Investment Analysis\n");
+            fclose(history);
+			system("clear");
+			invelyz();
+		}
+		if (opr == 15) {
+		    fputs(history, "User opened Differentiation of a Fucntion\n");
+            fclose(history);
+			system("clear");
+			pdig();
+		}
+	}
+	else {
+
+
+            if (opr == 0) {
+		system("clear");
+		main();
+            }
+
+
+	if (opr == 1) {
+            fputs(history, "User opened Distance between two points\n");
+            fclose(history);
+			system("clear");
+			dis_1();
+		}
+		if (opr == 2) {
+            fputs(history, "User opened Cartesian to Polar Conversion\n");
+            fclose(history);
+			system("clear");
+			pol();
+		}
+		if (opr == 3) {
+            fputs(history, "User opened Quadrant of a point\n");
+            fclose(history);
+			system("clear");
+			qua();
+		}
+		if (opr == 4) {
+		    fputs(history, "User opened Mirror of a point\n");
+            fclose(history);
+			system("clear");
+			mircor();
+		}
+		if (opr == 5) {
+		    fputs(history, "User opened Distance of a point from Origin\n");
+            fclose(history);
+			system("clear");
+			dis_2();
+		}
+		if (opr == 6) {
+		    fputs(history, "User opened Angle made by points\n");
+            fclose(history);
+			system("clear");
+			angle();
+		}
+		if (opr == 7) {
+		    fputs(history, "User opened Straight line equation generator\n");
+            fclose(history);
+			system("clear");
+			eqgen();
+		}
+		if (opr == 8) {
+		    fputs(history, "User opened Distance between two parallel straight lines\n");
+            fclose(history);
+			system("clear");
+			dis_st();
+		}
+		if (opr == 9) {
+		    fputs(history, "User opened Slope of a st line\n");
+            fclose(history);
+			system("clear");
+			slopeq();
+		}
+		if (opr == 10) {
+		    fputs(history, "User opened intersect points of st lines\n");
+            fclose(history);
+			system("clear");
+			isect();
+		}
+		if (opr == 11) {
+		    fputs(history, "User opened Maxima/Minima of a function\n");
+            fclose(history);
+			system("clear");
+			maxima();
+		}
+		if (opr == 12) {
+		    fputs(history, "User opened Profit diagram\n");
+            fclose(history);
+			system("clear");
+			diff();
+		}
+		if (opr == 13) {
+		    fputs(history, "User opened Integration of a Fucntion\n");
+            fclose(history);
+			system("clear");
+			ntgrate();
+		}
+		if (opr == 14) {
+		    fputs(history, "User opened Investment Analysis\n");
+            fclose(history);
+			system("clear");
+			invelyz();
+		}
+		if (opr == 15) {
+		    fputs(history, "User opened Differentiation of a Fucntion\n");
+            fclose(history);
+			system("clear");
+			pdig();
+		}
+
+
+	}
+}
+
+}
+
+wlcanon() {
+
+
+    char sentence[1000];
+	int opr;
+	printf("\n\t----------------\n\t      Menu\n\t----------------\n\n");
+	printf("\033[35m\t\e[1mCoordinate Geometry\033[0m\e[0m\n");
+	printf("\t\033[35m\e[1m[1]\033[0m\e[0m  Distance between two points\n");
+	printf("\t\033[35m\e[1m[2]\033[0m\e[0m  Polar to Cartesian Conversion\n");
+	printf("\t\033[35m\e[1m[3]\033[0m\e[0m  Quadrant of a Coordinate\n");
+	printf("\t\033[35m\e[1m[4]\033[0m\e[0m  Mirror of a point\n");
+	printf("\t\033[35m\e[1m[5]\033[0m\e[0m  Distance of a Coordinate from O(0,0)\n");
+	printf("\t\033[35m\e[1m[6]\033[0m\e[0m  Angle made by the point with respect to x & y axis.\n");
+	printf("\n\n\033[36m\t\e[1mStraight Lines\033[0m\e[0m\n");
+	printf("\t\033[36m\e[1m[7]\033[0m\e[0m  St. Line Equation Generator\n");
+	printf("\t\033[36m\e[1m[8]\033[0m\e[0m  Distance between two st. lines\n");
+	printf("\t\033[36m\e[1m[9]\033[0m\e[0m  Slope of a st. Line\n");
+	printf("\t\033[36m\e[1m[10]\033[0m\e[0m Intersect Point of two st. lines\n");
+	printf("\n\n\033[33m\t\e[1mCurves Analysis\033[0m\e[0m\n");
+	printf("\t\033[33m\e[1m[11]\033[0m\e[0m  Maxima & Minima of a Function\n");
+	printf("\t\033[33m\e[1m[12]\033[0m\e[0m  Differentiation of a Function\n");
+	printf("\t\033[33m\e[1m[13]\033[0m\e[0m  Area of a Function binded by x=a,y=b\n");
+	printf("\n\n\033[32m\t\e[1mBusiness Analysis\033[0m\e[0m\n");
+	printf("\t\033[32m\e[1m[14]\033[0m\e[0m  Invest Analysis\n");
+	printf("\t\033[32m\e[1m[15]\033[0m\e[0m  Profit Diagram\n");
+	printf("\n\n");
+	printf("\t\033[31m\e[1m[0]\033[0m\e[0m  Log Out / Exit\n\n");
+    while ( opr > 15) {
+	printf("\t---------------------\n\tChoose Operation: "); scanf("%d",&opr);
+	printf("\r---------------------\n");
+	if (opr > 15) {
+		printf("\n\n\tINVALID OPERATION!!!\n\n");
+		printf("\t---------------------\n\n\tChoose Operation: "); scanf("%d",&opr);
+	printf("\t---------------------\n");
+
+		if (opr == 0) {
+		system("clear");
+		main();
+	}
+
+
+		if (opr == 1) {
+			dis_1();
+		}
+		if (opr == 2) {
 			system("clear");
 			pol();
 		}
@@ -1024,6 +1429,7 @@ wlc() {
 			system("clear");
 			qua();
 		}
+
 		if (opr == 4) {
 			system("clear");
 			mircor();
@@ -1060,11 +1466,22 @@ wlc() {
 			system("clear");
 			diff();
 		}
+		if (opr == 13) {
+			system("clear");
+			ntgrate();
+		}
 		if (opr == 14) {
 			system("clear");
 			invelyz();
 		}
+		if (opr == 15) {
+			system("clear");
+			pdig();
+		}
+
 	}
+
+
 	else {
 
 
@@ -1074,13 +1491,9 @@ wlc() {
 	}
 
 		if (opr == 1) {
-            //fputs("User opened Distance between two points\n",history);
-            //fclose(history);
 			dis_1();
 		}
 		if (opr == 2) {
-            //fputs(history, "User opened Cartesian to Polar Conversion\n");
-            //fclose(history);
 			system("clear");
 			pol();
 		}
@@ -1125,9 +1538,17 @@ wlc() {
 			system("clear");
 			diff();
 		}
+		if (opr == 13) {
+			system("clear");
+			ntgrate();
+		}
 		if (opr == 14) {
 			system("clear");
 			invelyz();
+		}
+		if (opr == 15) {
+			system("clear");
+			pdig();
 		}
 
 	}
